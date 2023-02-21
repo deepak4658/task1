090ac1cdcdd5a28bd94f0a1fa861dd44fa10b8ed
Deepak Kumar <125899132+deepak4658@users.noreply.github.com>
2023-02-21 23:40:22 +0530

    Check the number

--- myfirstcode.java
+++ myfirstcode.java
@@ -5,3 +5,19 @@ public class Main
     System.out.println("Hello World!");
   }
 }
+
+// check the even number
+public class Main
+{
+  public static void main(String [] args)
+  {
+    int n=10;
+    if(n%2==0)
+    {
+      System.out.println("Number is Even");
+  }
+    else {
+      System.out.println("Number is odd");
+}
+  }
+
